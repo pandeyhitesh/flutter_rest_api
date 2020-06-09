cc1348db6b2f64536bcf8237c704fe5850444fee
hitesh <hiteshpandey206@gmail.com>
2020-06-09 15:52:32 +0530

    Initial commit

---
+++ lib/rest_api.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class URLS {
+  static const String BASE_URL = 'https://djangoapi-awkot.run-ap-south1.goorm.io/employees';
+}
+
+class ApiService {
+  static Future<List<dynamic>> getEmployees() async {
+    // RESPONSE JSON :
+    // [{
+    //   "id": "1",
+    //   "employee_name": "",
+    //   "employee_salary": "0",
+    //   "employee_age": "0",
+    //   "profile_image": ""
+    // }]
+    final response = await http.get('https://djangoapi-awkot.run-ap-south1.goorm.io/employees');
+    if (response.statusCode == 200) {
+      print(response.body);
+      return json.decode(response.body);
+    } else {
+      return null;
+    }
+  }
+
+  static Future<bool> addEmployee(body) async {
+    // BODY
+    // {
+    //   "name": "test",
+    //   "age": "23"
+    // }
+    final response = await http.post('${URLS.BASE_URL}/create', body: body);
+    if (response.statusCode == 200) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
